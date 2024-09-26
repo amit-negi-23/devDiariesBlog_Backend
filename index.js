dfd054fe71e76f7ae6cf71ea90d8c0a206a3ca83
amit-negi-23 <amitnegiit23@gmail.com>
2024-09-26 16:14:11 +0530

    code formatting

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
 import express from "express";
 import authRouter from "./routes/authRoute.js";
-import labelRouter from "./routes/labelRoute.js"
-import postRouter from "./routes/postRoute.js"
+import labelRouter from "./routes/labelRoute.js";
+import postRouter from "./routes/postRoute.js";
 import dbConnection from "./db/db.js";
 import dotenv from "dotenv";
-import cors from 'cors';
+import cors from "cors";
 
 const app = express();
 
 dotenv.config();
 dbConnection();
 
-app.use(cors({
-  origin: "*",
-}))
-app.use(express.urlencoded({extended: true}));
-app.use(express.json({limit: '*'}));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "*" }));
 app.use("/api", authRouter);
-app.use("/label",labelRouter)
-app.use("/post",postRouter)
-
-// app.post('/',()=>{
-//     console.log("hello world")
-// })
+app.use("/label", labelRouter);
+app.use("/post", postRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`App is running on ${process.env.PORT}`);
